7fc6dc395f79e428ed9e520a7fd8a8b5c53f237a
Aleksandar Rodić <aleks.rodic@pm.me>
2023-05-28 11:16:09 +0200

    Making --start-date required arg

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from strava import get_activities
 parser = argparse.ArgumentParser(description='Plot pace for Strava activities.')
 
 parser.add_argument('-t', '--type', choices=('Run', 'Ride'), default='Run', type=str, required=False, help='Activity type to plot')
-parser.add_argument('-s', '--start-date', type=datetime.date.fromisoformat, help='Start date for activities to plot in YYYY-MM-DD format')
+parser.add_argument('-s', '--start-date', type=datetime.date.fromisoformat, required=True, help='Start date for activities to plot in YYYY-MM-DD format')
 
 args = parser.parse_args()
 
